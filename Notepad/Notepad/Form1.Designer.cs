d139084d3245d82fd8ff6e93684cd5348fd966a7
Богдан Мысник <musnik1@outlook.com>
2021-10-11 14:35:36 +0500

    Revert "зеленая форма и нет нопки закрыть"
    This reverts commit cc83e931a23e5227fa8608496eddff74777f54f7.

--- Notepad/Notepad/Form1.Designer.cs
+++ Notepad/Notepad/Form1.Designer.cs
@@ -29,14 +29,27 @@ namespace Notepad
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(296, 113);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 55);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Закрыть ";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
             this.ClientSize = new System.Drawing.Size(768, 270);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Блокнот";
             this.ResumeLayout(false);
@@ -44,6 +57,8 @@ namespace Notepad
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
 
